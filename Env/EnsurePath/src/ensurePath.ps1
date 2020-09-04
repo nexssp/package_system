da14b6e86a2de7c04e1bbfdff984da30e93fcf25
Marcin Polak <mapoart@gmail.com>
2020-09-04 10:50:25 +0200

    Added nxsIn handling

--- Env/EnsurePath/src/ensurePath.ps1
+++ Env/EnsurePath/src/ensurePath.ps1
@@ -3,11 +3,11 @@ $input | . "$($env:NEXSS_PACKAGES_PATH)/Nexss/Lib/NexssIn.ps1"
 
 . "$($env:NEXSS_PACKAGES_PATH)/Nexss/Lib/Env.ps1"
 
-if (!$NexssStdout.EnvPathAdd) {
-    nxsError("EnvPathAdd is required. Specify Path you wish to add to the system.") 
+if (!$inFieldValue_1) {
+    nxsError("Parameter is required is required. Specify Path you wish to add to the system.") 
     exit
 }
-
-ensureEnvPath $NexssStdout.EnvPathAdd
+nxsInfo("Adding path $($inFieldValue_1) to the environment")
+ensureEnvPath $inFieldValue_1[0]
 
 . "$($env:NEXSS_PACKAGES_PATH)/Nexss/Lib/NexssOut.ps1"
