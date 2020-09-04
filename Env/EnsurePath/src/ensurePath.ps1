5391a0c12d3dee4249469ddb3afd51a1f1545afe
Marcin Polak <mapoart@gmail.com>
2020-09-04 12:34:33 +0200

    Better handling for Mac and Linux

--- Env/EnsurePath/src/ensurePath.ps1
+++ Env/EnsurePath/src/ensurePath.ps1
@@ -7,7 +7,7 @@ if (!$inFieldValue_1) {
     nxsError("Parameter is required is required. Specify Path you wish to add to the system.") 
     exit
 }
-nxsInfo("Adding path $($inFieldValue_1) to the environment")
+
 ensureEnvPath $inFieldValue_1[0]
 
 . "$($env:NEXSS_PACKAGES_PATH)/Nexss/Lib/NexssOut.ps1"
