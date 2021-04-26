1d19394f67715d5e380f5dd85eea06ed4573d800
Marcin Polak <mapoart@gmail.com>
2021-04-27 00:54:44 +0200

    Fix for array

--- Env/EnsurePath/Linux/ensurePath.sh
+++ Env/EnsurePath/Linux/ensurePath.sh
@@ -1,4 +1,14 @@
 NexssStdin=`cat`
+
+is_array()
+{   #detect if arg is an array, returns 0 on sucess, 1 otherwise
+    [ -z "$1" ] && return 1
+    if [ -n "$BASH" ]; then
+        declare -p ${1} 2> /dev/null | grep 'declare \-a' >/dev/null && return 0
+    fi
+    return 1
+}
+
 # NexssStdout=$(echo "$NexssStdin"|jq ".outputBash = \"Hello from Bash! $BASH_VERSION\"")
 # NexssStdout=$(echo "$NexssStdin"|jq -r ".test = \"test\"")
 folderToAdd=$(echo "$NexssStdin"|jq -r ".nxsIn")
@@ -8,18 +18,20 @@ if [[ "$(declare -p folderToAdd)" =~ "declare -a" ]]; then
     folderToAdd=$(echo "$NexssStdin"|jq -r ".nxsIn[0]")
 fi
 
-EnvPathAdd=$(echo "$NexssStdin"|jq -r '.EnvPathAdd')
-if [ -z $EnvPathAdd ]; then
-    folderToAdd=$EnvPathAdd
-fi
+folderToAdd=$(echo "$NexssStdin"|jq -r ".nxsIn[0]")
 
-if [ ! -d $folderToAdd ]
-then
-  echo "NEXSS/error:Folder $folderToAdd does not exist">&2
-  exit 1
-else
+# EnvPathAdd=$(echo "$NexssStdin"|jq -r '.EnvPathAdd')
+# if [ -z $EnvPathAdd ]; then
+    # folderToAdd=$EnvPathAdd
+ # fi
+
+if [ -d "$folderToAdd" ]; then
     echo "NEXSS/ok:Adding path to your PATH: $folderToAdd">&2
     # Adds only once as grep checks if exists
     grep -qxF "export PATH=\"$folderToAdd:\$PATH\"" ~/.bashrc || echo "export PATH=\"$folderToAdd:\$PATH\"" > ~/.bashrc
     echo $NexssStdout>&1
+else
+    echo "NEXSS/error:Folder $folderToAdd does not exist">&2
+    exit 1
 fi
+
